0d7ffa4c385c3863f085625098889ec9ea4406d0
Sundance <sundance@ierne.eu.org>
2019-09-24 12:59:46 +0100

    Added crude function to guess the default user.

--- zsh/conf.d/50-zsh-theme-powerlevel9k.zsh
+++ zsh/conf.d/50-zsh-theme-powerlevel9k.zsh
@@ -1,3 +1,20 @@
+# Helper function that we need to guess who owns this file here.
+function guess_default_user {
+  dir=${0:a:h}
+
+  while [ -n "$dir" -a "$dir" != "/" ] ; do
+    maybe_username="$(basename "$dir")"
+    if [ "$(basename "$(dirname "$dir")")" = "home" ] ; then
+      echo "$maybe_username"
+      exit
+    fi
+    dir="$(dirname "$dir")"
+  done
+
+  echo "?"
+}
+
+
 # If running a 256 color term, load theme engine powerlevel9k (Ubuntu package 'zsh-theme-powerlevel9k'):
 [ $(echotc Co) -eq '256' ] && \
   [ -f /usr/share/powerlevel9k/powerlevel9k.zsh-theme ] && source /usr/share/powerlevel9k/powerlevel9k.zsh-theme
@@ -9,7 +26,7 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(root_indicator context dir dir_writable vcs p
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs time)
 
 # 'context': Default user. Hide element when we're that person.
-DEFAULT_USER=sundance
+DEFAULT_USER="$(guess_default_user)"
 
 # 'dir': Max directories to show:
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=2

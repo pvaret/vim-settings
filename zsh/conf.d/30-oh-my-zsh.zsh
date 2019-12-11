09f964c04bc9072efd533bf66eb744ccfe1d8c2b
P. Varet <p.varet@gmail.com>
2019-12-11 18:18:57 +0000

    Disabled auto-correction. It's too buggy.

--- zsh/conf.d/30-oh-my-zsh.zsh
+++ zsh/conf.d/30-oh-my-zsh.zsh
@@ -10,7 +10,8 @@ if [ -f "$ZSH/oh-my-zsh.sh" ] ; then
   HYPHEN_INSENSITIVE="true"
 
   # Uncomment the following line to enable command auto-correction.
-  ENABLE_CORRECTION="true"
+  # DISABLED for now: it often try to correct things that are not broken.
+  #ENABLE_CORRECTION="true"
 
   # Uncomment the following line to display red dots whilst waiting for completion.
   COMPLETION_WAITING_DOTS="true"

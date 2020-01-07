460705a8c6bca4e215d4c167b20080390ecf86b0
Sundance <sundance@ierne.eu.org>
2020-01-07 16:54:35 +0000

    Minor improvement to zsh history management.

--- zsh/conf.d/05-options.zsh
+++ zsh/conf.d/05-options.zsh
@@ -1,2 +1,7 @@
 # Allow comments in interactive mode:
 setopt interactivecomments
+
+# Update the shared history file after each command. Each shell still only
+# keeps its own history though, but newly started shells will have all the
+# current history.
+setopt inc_append_history_time

fec415942b837d1d67f4121c3ae9ebfb78c41663
Sundance <sundance@ierne.eu.org>
2020-05-17 23:43:50 +0100

    Don't share zsh history between terms even if Oh My ZSH wants to.

---
+++ zsh/conf.d/95-options.zsh
@@ -0,0 +1,5 @@
+# Late options. A good place to override questionable options from plugins
+# such as Oh My ZSH.
+
+# Don't actually share history between still running terminals. That's silly.
+unsetopt share_history

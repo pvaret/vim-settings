25cc428a358f4a5f0ceea1a612e4c7651368a4cc
Sundance <sundance@ierne.eu.org>
2020-10-15 22:52:23 +0100

    Added alias to make fold more useful by default.

--- zsh/conf.d/05-aliases.zsh
+++ zsh/conf.d/05-aliases.zsh
@@ -1,3 +1,7 @@
 # Aliases:
 alias ls='ls --color'
 alias tm='tmux attach || tmux new-session -s main'
+
+# fold is a cool tool to wrap text, but by default it split words. Let it wrap
+# at whitespace instead.
+alias fold='fold --spaces'

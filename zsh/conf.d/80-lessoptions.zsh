329aa3b9d9fe87ff2eb2ee3436da6e72b8c9a38d
P. Varet <p.varet@gmail.com>
2025-03-17 10:36:11 +0000

    Updated less options.

--- zsh/conf.d/80-lessoptions.zsh
+++ zsh/conf.d/80-lessoptions.zsh
@@ -1,4 +1,5 @@
 # Enable ANSI colors.
+# Quit right away if there's only one screen of text to show.
 # Add a prompt at the bottom showing the current position in the file in terms
 # of lines.
-export LESS="$LESS -R -P%lb/%L"
+export LESS="-R --quit-if-one-screen -P%lb/%L"

fb46ef4d83b11189ac542007c3f74bdf7cdb4162
P. Varet <p.varet@gmail.com>
2025-11-01 23:34:52 +0000

    Switched ZSH theme to powerlevel10k.

--- zsh/conf.d/30-oh-my-zsh.zsh
+++ zsh/conf.d/30-oh-my-zsh.zsh
@@ -1,8 +1,6 @@
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
-ZSH_THEME=jonathan
-
 if [ -f "$ZSH/oh-my-zsh.sh" ] ; then
 
   # See ~/.oh-my-zsh/templates/zshrc.zsh-template for configuration options.
@@ -52,5 +50,22 @@ if [ -f "$ZSH/oh-my-zsh.sh" ] ; then
     zsh-interactive-cd
   )
 
+  if [ -f "$ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme" ] ; then
+    ZSH_THEME=powerlevel10k/powerlevel10k
+  else
+    ZSH_THEME=jonathan
+  fi
+
+  # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+  # Initialization code that may require console input (password prompts, [y/n]
+  # confirmations, etc.) must go above this block; everything else may go below.
+  if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+  fi
+
   source "$ZSH/oh-my-zsh.sh"
+
+  # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+  [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
 fi
